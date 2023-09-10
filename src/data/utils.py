9bad827510109f316a9ab51deba3da0fbe4d78ca
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-10 21:05:33 +0900

    added exported list file

--- src/data/utils.py
+++ src/data/utils.py
@@ -5,8 +5,22 @@ FILE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 # path to get collected data
 TARGET_PATH = os.path.join (FILE_DIR, 'data/interim')
 
-data_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data/interim'))
-def save_df_to_csv (df: pd.DataFrame, target_path, filename, prefix = ''):
+def _make_file_list (filename: str, target_path, content: list):
+    """
+    """
+    # file_path = os.path.join(data_folder, filename)
+    os.makedirs (target_path, exist_ok= True)
+    if not filename.endswith ('.txt'): filename += '.txt'
+    output_file = os.path.join (target_path, filename)
+    
+    with open (output_file, 'w') as file:
+        for line in content:
+            file.write (line)
+            file.write ('\n')
+    print ('List of exported files saved at: ', output_file)
+    
+
+def save_df_to_csv (df: pd.DataFrame, target_path, filename, prefix = '', postfix = '', output_list_file = None):
     os.makedirs (target_path, exist_ok = True)
     if not (prefix == '') and (not prefix.endswith ('_')): prefix += '_'
     if not (postfix == '') and (not prefix.startswith ('_')): postfix = '_' + postfix
@@ -16,7 +30,10 @@ def save_df_to_csv (df: pd.DataFrame, target_path, filename, prefix = ''):
     output_file = os.path.join(target_path, filename)
     df.to_csv (output_file, index = False)
     print ("Finished: files saved to", target_path)
-    print ("\t", filename, sep = '')
+    print ("Saved:\t",  filename, sep = '')
+    
+    if output_list_file is not None:
+        _make_file_list (filename= filename, target_path=target_path, content= [filename])
 
 def batch_save_df_to_csv (file_name_dfs: dict, target_path, prefix ='', postfix ='',output_list_file = None):
     """
@@ -47,18 +64,4 @@ def batch_save_df_to_csv (file_name_dfs: dict, target_path, prefix ='', postfix 
     if output_list_file is not None:
         # make a txt file containing the names of exported file
         _make_file_list (filename = output_list_file, target_path=target_path, content=content)
-    
-def _make_file_list (filename: str, target_path, content: list):
-    """
-    """
-    # file_path = os.path.join(data_folder, filename)
-    os.makedirs (target_path, exist_ok= True)
-    if not filename.endswith ('.txt'): filename += '.txt'
-    output_file = os.path.join (target_path, filename)
-    
-    with open (output_file, 'w') as file:
-        for line in content:
-            file.write (line)
-            file.write ('\n')
-    print ('List of exported files saved at: ', output_file)
     
