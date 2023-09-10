5ad0cd9be4db1b8e144533781ee5650e56f802ab
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-10 12:11:10 +0900

    preprocess: updated build_feature

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,15 +2,17 @@ import sys
 import os
 sys.path.append("..")
 import argparse
-from src.data import ingestion, cleaning, build_features
+from src.data.ingestion import collect_data
+from src.data.cleaning import clean_data
+from src.data.build_features import build_features
 
 ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 TARGET_PATH = os.path.join(ROOT_FOLDER, 'data/interim')
 
 def main():
-    ingestion.collect_data()
-    cleaning.clean_data(target_path= TARGET_PATH)
-    technique_features, group_features = build_features.build_features()
+    collect_data()
+    clean_data(target_path= TARGET_PATH)
+    technique_features, group_features = build_features()
     
 if __name__ == '__main__':
     main()
