d31f86924e1d944e9993bd46d83c54525a3124dd
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-11 19:59:43 +0900

    option to return dataset feature sizes

--- src/models/model1/dataloader.py
+++ src/models/model1/dataloader.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-from .constants import ROOT_FOLDER, TRAIN_DATASET_FILENAME, CV_DATASET_FILENAME, TEST_DATASET_FILENAME, RANDOM_STATE
+from .constants import ROOT_FOLDER, TRAIN_DATASET_FILENAME, CV_DATASET_FILENAME, TEST_DATASET_FILENAME
+from .constants import RANDOM_STATE
+from .constants import INPUT_GROUP_LAYER_NAME, INPUT_TECHNIQUE_LAYER_NAME
+
 ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 SOURCE_PATH = os.path.join (ROOT_FOLDER, 'data/processed')
 
@@ -15,7 +18,7 @@ def _single_load (file_name):
     print('DataSet size:\t', len(dataset), 'examples')
     return dataset
 
-def load_data (sample_train: float = None):
+def load_data (sample_train: float = None, feature_info = True):
     """
     sample_train: option to sample and train only a fraction of train_dataset
     """
@@ -25,6 +28,14 @@ def load_data (sample_train: float = None):
     
     if sample_train is not None:
         num_samples = int(len(train_dataset) * sample_train)
-        train_dataset = train_dataset.shuffle(buffer_size=len(train_dataset), seed=RANDOM_STATE).take(num_samples)
+        print ('Train dataset is sampled:',num_samples, 'examples')
+        sampled_train_dataset = train_dataset.shuffle(buffer_size=len(train_dataset), seed=RANDOM_STATE).take(num_samples)
         
-    return train_dataset, cv_dataset, test_dataset
+    if feature_info:
+        info = {
+            'X_group_num_features' : train_dataset.element_spec[0][INPUT_GROUP_LAYER_NAME].shape[1],
+            'X_technique_num_features' : train_dataset.element_spec[0][INPUT_TECHNIQUE_LAYER_NAME].shape[1]
+        }
+        return sampled_train_dataset, cv_dataset, test_dataset, info
+    
+    return sampled_train_dataset, cv_dataset, test_dataset
