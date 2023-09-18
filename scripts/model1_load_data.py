58c172886f17d79a7bbc1eb98c680c7deda4ee35
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-18 22:53:42 +0900

    train model from config

--- scripts/model1_load_data.py
+++ scripts/model1_load_data.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from src.models.model1.dataloader import load_data
 
 def main():
+    ## parsing arguments
     parser = argparse.ArgumentParser (description= 'command-line arguments when running {}'.format ('__file__'))
     parser.add_argument ('--sample_train','-st', 
                          type = float, 
@@ -14,6 +15,7 @@ def main():
                          help='optional argument to sample a traction of train_dataset for training')
     args = parser.parse_args()
     sample_train = args.sample_train
+    
     load_data (sample_train= sample_train)
     
 if __name__ == '__main__':
