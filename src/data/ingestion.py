dc26d280fc095e86a9b9ad05fe3d2bbbf59f3ad8
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-20 10:48:45 +0900

    file clean up

--- src/data/ingestion.py
+++ src/data/ingestion.py
@@ -3,46 +3,12 @@ last update: 2023-09-08
 Used to gather the data from MITRE ATT&CK. It extracts the following pandas DataFrames and save as csv files in "data/interim"
 
 collected_technique_df.csv: list of all Techniques
-collected_techni ques_mitigations_df.csv: list of all Mitigations for each Techniques
+collected_techniques_mitigations_df.csv: list of all Mitigations for each Techniques
 collected_groups_df.csv: list of all Groups
 collected_groups_techniques_df.csv: list of all Techniques used by each Group
 collected_groups_software_df.csv: list of all Software used by each Group
 """
 
-### 2023-09-06 default code, update later
-
-# -*- coding: utf-8 -*-
-# import click
-# import logging
-# from pathlib import Path
-# from dotenv import find_dotenv, load_dotenv
-
-
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """ Runs data processing scripts to turn raw data from (../raw) into
-#         cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info('making final data set from raw data')
-
-
-# if __name__ == '__main__':
-#     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
-
-#     # find .env automagically by walking up directories until it's found, then
-#     # load up the .env entries as environment variables
-#     load_dotenv(find_dotenv())
-
-#     main()
-
-### end of default code
 
 from stix2 import MemoryStore
 import mitreattack.attackToExcel.stixToDf as stixToDf
