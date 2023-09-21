b0ef2f15f4f05b874687878d2ee86affc118e914
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-21 14:58:21 +0900

    updated comments / use constants

--- src/data/aligning.py
+++ src/data/aligning.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from . import utils
+from .constants import GROUP_ID_NAME, TECHNIQUE_ID_NAME, LABEL_NAME
 
 ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 # path to get cleaned data
@@ -25,23 +26,23 @@ def align_input_to_target(feature_df: pd.DataFrame, object: str, target_df: pd.D
         
     """
     print (PROCESS_RUNNING_MSG)
-    id_name = ''
-    filename = ''
+    id_name = '' # for mergin
+    filename = '' # for saving table after aligning
     if from_set in ('train', 'cv','test'):
         filename = from_set + '_'
     if object == 'group':
-        id_name = 'group_ID'
+        id_name = GROUP_ID_NAME
         filename += 'X_group'
     elif object == 'technique':
-        id_name = 'technique_ID'
+        id_name = TECHNIQUE_ID_NAME
         filename += 'X_technique'
     df_aligned = pd.merge(left = feature_df, right= target_df, on = id_name, how = 'right')
     
     # remove unecessary columns after merging
     if object == 'group':
-        df_aligned.drop (columns= ['technique_ID', 'target'], inplace= True)
+        df_aligned.drop (columns= [TECHNIQUE_ID_NAME, LABEL_NAME], inplace= True)
     elif object == 'technique':
-        df_aligned.drop (columns= ['group_ID', 'target'], inplace= True)
+        df_aligned.drop (columns= [GROUP_ID_NAME, LABEL_NAME], inplace= True)
     
     if save_to_csv:
         utils.save_df_to_csv (df_aligned, target_path = TARGET_PATH, filename= filename, postfix=TARGET_POSTFIX)
--- src/data/balancing.py
+++ src/data/balancing.py
@@ -1,10 +1,11 @@
 """
-Last update 2023-09-20
+Last update 2023-09-21
 Balance the labels in a pandas Dataframe.
 """
 import os
 import pandas as pd
 from . import utils
+from .constants import GROUP_ID_NAME, TECHNIQUE_ID_NAME, LABEL_NAME
 from imblearn.over_sampling import RandomOverSampler
 
 ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
@@ -20,13 +21,13 @@ RANDOM_STATE = 13
 
 def naive_random_oversampling (df: pd.DataFrame, save_as_csv = True):
     """
-    Balance the labels stored in 'target' column of a DataFrame 'df'
-    Currently this only works if 'df' has EXACTLY 3 columns: 'group_ID', 'technique_ID', and 'target'
+    Balance the labels stored in `LABEL_NAME` column of a DataFrame 'df'
+    Currently this only works if 'df' has EXACTLY 3 columns: `GROUP_ID_NAME`, `TECHNIQUE_ID_NAME`, and `LABEL_NAME`
     """
     print (PROCESS_RUNNING_MSG)
     postfix = 'naive_oversampled'
-    X = df[['group_ID','technique_ID']]
-    y = df[['target']]
+    X = df[[GROUP_ID_NAME, TECHNIQUE_ID_NAME]]
+    y = df[[LABEL_NAME]]
     ros = RandomOverSampler(random_state= RANDOM_STATE)
     x_resampled,y_resampled = ros.fit_resample(X , y)
     res_df = pd.concat ([x_resampled,y_resampled], axis =1)
