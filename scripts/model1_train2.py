7b21390007c8f6c4537ce8d237c9af8ec6c6d325
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-09-26 16:20:50 +0900

    edit train script for updated modules

--- scripts/model1_train2.py
+++ scripts/model1_train2.py
@@ -15,12 +15,10 @@ ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 CONFIG_FOLDER = os.path.join (ROOT_FOLDER, 'configs')
 ### folder for config of single model train
 SINGLE_TRAIN_FOLDER_NAME = 'model1_single_train'
-
 REPORT_FOLDER = os.path.join (ROOT_FOLDER, 'reports', 'model1')
-TRAINED_MODELS_FOLDER = os.path.join (ROOT_FOLDER, 'trained_models')
+TRAINED_MODELS_FOLDER = os.path.join (ROOT_FOLDER, 'trained_models', 'model1')
 
-from src.models.model1.archive.preprocessing import model_preprocess
-from src.models.model1.dataloader import load_data
+from src.models.model1.dataloader import load_train_datasets
 from src.models.model1.model_v0_4 import Model1
 def main():
     #### PARSING ARGS
@@ -29,16 +27,12 @@ def main():
                          type = str, 
                          help='name of config file to build and train the model')
     
-    parser.add_argument ('-pp', '--preprocess', type = bool,
-                         required= False, default= False,
-                         help = 'option to preprocess the data first (only necessary for the first time)')
     args = parser.parse_args()
-    config_file_name = args.config
-    preprocess = args.preprocess
+    config_filename = args.config
 
     #### LOAD CONFIGS FROM CONFIG FILE
-    if not config_file_name.endswith ('.yaml'): config_file_name += '.yaml'
-    config_file_path = os.path.join (CONFIG_FOLDER, SINGLE_TRAIN_FOLDER_NAME,config_file_name)
+    if not config_filename.endswith ('.yaml'): config_filename += '.yaml'
+    config_file_path = os.path.join (CONFIG_FOLDER, SINGLE_TRAIN_FOLDER_NAME,config_filename)
     with open (config_file_path, 'r') as config_file:
         config = yaml.safe_load (config_file)
         
@@ -46,21 +40,19 @@ def main():
     train_config = config['train']
     batch_size = train_config['batch_size']
     epochs = train_config['epochs']
-    learning_rate = train_config['learning_rate']
-    print ('---config for Model1\n',config)
+    learning_rate = float (train_config['learning_rate'])
+    formatted_text = yaml.dump(config, default_flow_style=False, indent=2, sort_keys=False)
+    print ('---config for Model1\n',formatted_text)
 
     #### LOAD DATASETS, THEN CONFIG DATASETS
-    if preprocess: model_preprocess(train_set_split= 0.7)
-    train_dataset, train_cv_dataset, cv_dataset, test_dataset, feature_info = load_data(load_train_cv_set= True)
+    train_dataset, train_cv_dataset, feature_info = load_train_datasets()
     
     train_dataset = train_dataset.batch(batch_size)
     train_dataset = train_dataset.shuffle(buffer_size=len(train_dataset))
     train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
-
     train_cv_dataset = train_cv_dataset.batch(32)
-    # test_dataset = test_dataset.batch(32)
-    
+
     #### LOAD/COMPILE MODEL THEN TRAIN MODEL
     model = Model1 (input_sizes= feature_info,
                 config=model_architecture_config)    
@@ -69,7 +61,7 @@ def main():
     loss = keras.losses.BinaryCrossentropy (from_logits= True)
     model.compile (optimizer, loss = loss)
     
-    ## TRAIN MODEL ☝️ needs to add/fix more things if oversampling problem is true
+    #### TRAIN MODEL 
     start_time = time.time()
     history = model.fit (
         train_dataset,
@@ -85,11 +77,21 @@ def main():
     #### SAVE HISTORY
     history_df = pd.DataFrame(history.history)
     file_name = '{config_file}.csv'.format(config_file = args.config)
+    if not os.path.exists(os.path.join (REPORT_FOLDER, 'train_loss', SINGLE_TRAIN_FOLDER_NAME)):
+        os.makedirs(os.path.join (REPORT_FOLDER, 'train_loss', SINGLE_TRAIN_FOLDER_NAME))
+    
     file_path = os.path.join (REPORT_FOLDER, 'train_loss', SINGLE_TRAIN_FOLDER_NAME,file_name)
     history_df.to_csv(file_path, index=False)
     
+    ### SAVE TRAINED MODEL
     
-    #### SAVE TRAINED MODEL: tk
+    base_config_filename = config_filename.split(".")[0]
+    model_file_name = base_config_filename
+    if not os.path.exists(TRAINED_MODELS_FOLDER):
+        os.makedirs(TRAINED_MODELS_FOLDER)
+    model_file_path = os.path.join (TRAINED_MODELS_FOLDER, model_file_name)
+    model.save (model_file_path)
+    
     
 if __name__ == '__main__':
     main()
--- src/models/model1/dataloader.py
+++ src/models/model1/dataloader.py
@@ -7,26 +7,44 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-from .constants import ROOT_FOLDER, TRAIN_DATASET_FILENAME, CV_DATASET_FILENAME, TEST_DATASET_FILENAME
-from .constants import RANDOM_STATE
-from .constants import INPUT_GROUP_LAYER_NAME, INPUT_TECHNIQUE_LAYER_NAME
+from ...constants import TRAIN_DATASET_FILENAME, TRAIN_CV_DATASET_FILENAME, CV_DATASET_FILENAME, TEST_DATASET_FILENAME
+from ...constants import INPUT_GROUP_LAYER_NAME, INPUT_TECHNIQUE_LAYER_NAME
+from ...constants import RANDOM_STATE
 
 ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
-SOURCE_PATH = os.path.join (ROOT_FOLDER, 'data/processed')
+SOURCE_PATH = os.path.join (ROOT_FOLDER, 'data/processed/model1')
 
 def _single_load (file_name):
     print ("loading\t", file_name, '-----', end='')
     file_path = os.path.join (SOURCE_PATH, file_name) 
     dataset = tf.data.Dataset.load (file_path)
-    print ('loaded:')
+    print ('loaded')
     return dataset
 
-def load_data (sample_train: float = None, load_train_cv_set: bool = False):
+def load_train_datasets (sample_train: float = None):
+    train_dataset = _single_load (TRAIN_DATASET_FILENAME)
+    train_cv_dataset = _single_load (TRAIN_CV_DATASET_FILENAME)
+    
+    if sample_train is not None:
+        num_samples = int(len(train_dataset) * sample_train)
+        train_dataset = train_dataset.shuffle(buffer_size=len(train_dataset), seed=RANDOM_STATE).take(num_samples)
+        
+    print ('train_dataset: {} examples'.format(len(train_dataset)))
+    print ('train_cv_dataset: {} examples'.format(len(train_cv_dataset)))
+    
+    feature_info = {
+        'group_feature_size' : train_dataset.element_spec[0][INPUT_GROUP_LAYER_NAME].shape[0],
+        'technique_feature_size' : train_dataset.element_spec[0][INPUT_TECHNIQUE_LAYER_NAME].shape[0]
+    }
+    
+    return train_dataset, train_cv_dataset, feature_info
+
+def load_datasets (sample_train: float = None):
     """
     sample_train: option to sample and train only a fraction of train_dataset
     """
     train_dataset = _single_load (TRAIN_DATASET_FILENAME)
-    train_cv_dataset = None
+    train_cv_dataset = _single_load (TRAIN_CV_DATASET_FILENAME)
     cv_dataset = _single_load (CV_DATASET_FILENAME)
     test_dataset = _single_load (TEST_DATASET_FILENAME)
     
@@ -35,18 +53,15 @@ def load_data (sample_train: float = None, load_train_cv_set: bool = False):
         train_dataset = train_dataset.shuffle(buffer_size=len(train_dataset), seed=RANDOM_STATE).take(num_samples)
         # print (train_dataset.element_spec)
     
-    if load_train_cv_set: 
-        train_cv_dataset = _single_load ('train_cv_dataset')
     print ('train_dataset: {} examples'.format(len(train_dataset)))
     print ('train_cv_dataset: {} examples'.format(len(train_cv_dataset)))
     print ('cv_dataset: {} examples'.format(len(cv_dataset)))
-    print (('test_dataset: {} examples').format(len(test_dataset)))    
+    print ('test_dataset: {} examples'.format(len(test_dataset)))
     
     # return feature sizes to configure the model
     feature_info = {
         'group_feature_size' : train_dataset.element_spec[0][INPUT_GROUP_LAYER_NAME].shape[0],
         'technique_feature_size' : train_dataset.element_spec[0][INPUT_TECHNIQUE_LAYER_NAME].shape[0]
     }
-    
     
     return train_dataset, train_cv_dataset, cv_dataset, test_dataset, feature_info
